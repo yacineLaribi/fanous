d602c031184aac7dfb6fd548105d23523ffa95e2
unknown <lyacine230@gmail.com>
2024-02-16 01:20:23 +0100

    first commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -300,7 +300,7 @@
             $w = jQuery(window),
             viewTop = $w.scrollTop(),
             viewBottom = viewTop + $w.height(),
-            _top = $t.offset().top,
+            _top = $t.offset(),
             _bottom = _top + $t.height(),
             compareTop = partial === true ? _bottom : _top,
             compareBottom = partial === true ? _top : _bottom;
